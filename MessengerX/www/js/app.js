06260bcc01ae9406b78345e3fe518794b3cf3deb
Anirudhk94 <k.anirudh8@gmail.com>
2017-02-07 17:05:01 +0530

    Added logic to populate input on double tap

--- MessengerX/www/js/app.js
+++ MessengerX/www/js/app.js
@@ -55,6 +55,7 @@ app.controller('chatController', function($scope,$stateParams, $http, $ionicHist
  
   $scope.goBack = function(){
     $ionicHistory.goBack();
+    $scope.data = "";
   }
   var api = 'http://localhost:3000/friends/';
 
@@ -63,96 +64,13 @@ app.controller('chatController', function($scope,$stateParams, $http, $ionicHist
     console.log(data);
   });
 
-});
-
-
-
-
-///////////////////////////////////////////////////////////////////////////////////////////////
-
-app.directive('input', function($timeout) {
-  return {
-    restrict: 'E',
-    scope: {
-      'returnClose': '=',
-      'onReturn': '&',
-      'onFocus': '&',
-      'onBlur': '&'
-    },
-    link: function(scope, element, attr) {
-      element.bind('focus', function(e) {
-        if (scope.onFocus) {
-          $timeout(function() {
-            scope.onFocus();
-          });
-        }
-      });
-      element.bind('blur', function(e) {
-        if (scope.onBlur) {
-          $timeout(function() {
-            scope.onBlur();
-          });
-        }
-      });
-      element.bind('keydown', function(e) {
-        if (e.which == 13) {
-          if (scope.returnClose) element[0].blur();
-          if (scope.onReturn) {
-            $timeout(function() {
-              scope.onReturn();
-            });
-          }
-        }
-      });
-    }
+  $scope.onDoubleTap = function(message) {
+    console.log(message);
+    $scope.data = message;
   }
-})
-
-app.controller('Messages', function($scope, $timeout, $ionicScrollDelegate) {
-  console.log("In messages controller")
-  $scope.hideTime = true; 
-
-  var alternate,
-    isIOS = ionic.Platform.isWebView() && ionic.Platform.isIOS();
-
-  $scope.sendMessage = function() {
-    alternate = !alternate;
-
-    var d = new Date();
-  d = d.toLocaleTimeString().replace(/:\d+ /, ' ');
-
-    $scope.messages.push({
-      userId: alternate ? '12345' : '54321',
-      text: $scope.data.message,
-      time: d
-    });
-
-    delete $scope.data.message;
-    $ionicScrollDelegate.scrollBottom(true);
-
-  };
-
-
-  $scope.inputUp = function() {
-    if (isIOS) $scope.data.keyboardHeight = 216;
-    $timeout(function() {
-      $ionicScrollDelegate.scrollBottom(true);
-    }, 300);
-
-  };
-
-  $scope.inputDown = function() {
-    if (isIOS) $scope.data.keyboardHeight = 0;
-    $ionicScrollDelegate.resize();
-  };
-
-  $scope.closeKeyboard = function() {
-    // cordova.plugins.Keyboard.close();
-  };
-
-
-  $scope.data = {};
-  $scope.myId = '12345';
-  $scope.messages = [];
 
 });
+
+
+
+
